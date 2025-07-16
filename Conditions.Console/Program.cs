e2a282333eb3a7eb202b2cb663fe95e6740a2185
giapdn <donguyengiapicloud@gmail.com>
2025-07-16 17:32:37 +0700

    if, else, else if, ternaty, switch

---
+++ Conditions.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+  public static void Main(string[] args)
+  {
+    int x = 20;
+    int y = 18;
+    if (x > y)
+    {
+      Console.WriteLine("x is greater than y");
+    }
+
+    //Toán tử ba ngôi:
+
+    Console.WriteLine((x > y) ? "ok" : "fack");
+
+
+    //Switch
+
+    switch (x)
+    {
+      case 20:
+        Console.WriteLine($"Text");
+
+        break;
+
+      default:
+        Console.WriteLine($"d");
+
+        break;
+    }
+
+  }
+}

fa5366530316adf39c8859d901156a79d0889084
giapdn <donguyengiapicloud@gmail.com>
2025-07-16 16:52:31 +0700

    Console.ReadLine(), Convert.ToType

---
+++ GetUserInput.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program
+{
+  public static void Main(string[] args)
+  {
+    // Console.WriteLine($"Enter your name:");
+    // string fullName = Console.ReadLine();
+    // Console.WriteLine($"Your name is: {fullName}");
+
+
+    Console.WriteLine($"Enter a number: ");
+    double num = Convert.ToDouble(
+      Console.ReadLine()
+    );
+
+    Console.WriteLine($"Your number is {Math.Abs(num)}");
+
+
+
+
+  }
+}
